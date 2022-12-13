12a7dd543bb438dcdd5c37bea6ce8c3411846bc5
ankita9809 <k.ankita037@gmail.com>
2022-12-13 19:38:42 +0530

    Layout

---
+++ src/components/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import RocketLaunch from "../assets/1.jpg"
+
+export default function AddDevice() {
+    return (
+        <div className='grid grid-cols-1 sm:grid-cols-2'>
+            <div>
+                <h2>Add device</h2>
+                <div>circle</div>
+                <h5>Please select a devices to add</h5>
+                <img src={RocketLaunch} alt="RocketLaunch" />
+
+
+                <div>
+                    <form action="">
+                        <div>
+                            <label>Device name</label>
+                            <input type="text" />
+                        </div>
+                        <div>
+                            <label>Device video</label>
+                            <input type="text" />
+                        </div>
+                        <div>
+                            <label>Audio device</label>
+                            <input type="text" />
+                        </div>
+
+                    </form>
+                        <div>Line</div>
+                    <div>
+                        <button>Add device</button>
+                        <button>Cancel</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+} 
